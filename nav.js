0cf957e0b2bc002fe61e673de3e1bd4cebb2ee4a
jasperhauser <jasper.rh@gmail.com>
2023-01-29 20:40:37 +0100

    Code wat simpeler

--- nav.js
+++ nav.js
@@ -1,15 +1,15 @@
 
 // nav bar more (hamburger) menu behavior
 document.getElementById("nav-more").onclick = function(){
-    document.getElementById("body").classList.toggle('open'); // let's make this things smooth
+    document.body.classList.toggle('open'); // let's make this things smooth
 };
 // close nav when any tap/click registers on it
 document.getElementById("nav-menu-list").onclick = function(){
-    document.getElementById("body").classList.remove('open'); // let's make this things smooth
+    document.body.classList.remove('open'); // let's make this things smooth
 };
 // close nav when any tap/click registers on it
 document.getElementById("nav-menu-backdrop").onclick = function(){
-    document.getElementById("body").classList.remove('open'); // let's make this things smooth
+    document.body.classList.remove('open'); // let's make this things smooth
 };
 
 // scroll &	resize event listener
@@ -29,7 +29,7 @@ function throttle(fn, wait) {
 
 function onScroll(){
     // only show/hide nav bar when on home page
-    var home = document.getElementById("body").classList.contains("home");
+    var home = document.body.classList.contains("home");
     if (home) {
         // nav title bar show and hide
         var mastHeight = document.getElementById("masthead-photo").offsetHeight;
@@ -38,7 +38,7 @@ function onScroll(){
             document.getElementById("nav").classList.add("sticky");
         } else if (window.pageYOffset < (mastHeight / 2)) {
             document.getElementById("nav").classList.remove("sticky");
-            document.getElementById("body").classList.remove('open');
+            document.body.classList.remove('open');
         } else if (window.pageYOffset <= 5){
             document.getElementById("nav").classList.remove("sticky");
         }
